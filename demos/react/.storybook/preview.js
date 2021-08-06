feb3b4d5f0462bf7d75b88df59d49371eabbf878
Cory Reed <swashcap@gmail.com>
2021-08-05 23:30:46 -0700

    feat: add icon stories

--- demos/react/.storybook/preview.js
+++ demos/react/.storybook/preview.js
@@ -1,9 +1,2 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import "normalize.css/normalize.css";
+
