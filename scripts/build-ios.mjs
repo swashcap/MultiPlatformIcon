1ee7729332a513239785e93c3dfc89408d6fe56e
John Regner <regnerjr@gmail.com>
2021-08-18 11:48:37 -0700

    Update build-ios.mjs

--- scripts/build-ios.mjs
+++ scripts/build-ios.mjs
@@ -43,7 +43,7 @@ const ASSETS_DIRNAME = path.join(__dirname, "../ios/Sources/MultiPlatformIcon/Re
       ),
       fs.writeFile(
         path.join(__dirname, "../ios/Package.swift"),
-        `// swift-tools-version:5.0
+        `// swift-tools-version:5.3
 // This file is generated!
 import PackageDescription
 
@@ -88,18 +88,11 @@ ${svgs
 import Foundation
 import UIKit
 
-// Get the current bundle:
-// https://forums.swift.org/t/unable-to-find-bundle-in-package-target-tests-when-package-depends-on-another-package-containing-resources-accessed-via-bundle-module/43974/5
-private class CurrentBundleFinder {}
-
 public extension UIImage {
-    static let iconBundle = Bundle(for: CurrentBundleFinder.self)
-
     convenience init(icon multiPlatformIconAsset: MultiPlatformIconAsset) {
       self.init(
         named: multiPlatformIconAsset.resourceString,
-        in: UIImage.iconBundle,
-        compatibleWith: nil
+        in: Bundle.module
       )!
   }
 }`
